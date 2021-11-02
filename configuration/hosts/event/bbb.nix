50299663d85183385b39c5767825029c26fdfddc
Finn Behrens <me@kloenk.dev>
2021-11-02 11:30:13 +0100

    fix groups

--- configuration/hosts/event/bbb.nix
+++ configuration/hosts/event/bbb.nix
@@ -19,4 +19,34 @@
 
     petabyte.secrets."greenlight".owner = "greenlight";
     users.users.greenlight.extraGroups = [ "keys" ];
+
+    users.users.bbb-akka-apps.group = "bbb-akka-apps";
+    users.groups.bbb-akka-apps = {};
+
+    users.users.bbb-akka-fsesl.group = "bbb-akka-fsesl";
+    users.groups.bbb-akka-fsesl = {};
+
+    users.users.bbb-etherpad-lite.group = "bbb-etherpad-lite";
+    users.groups.bbb-etherpad-lite = {};
+
+    users.users.bbb-html5.group = "bbb-html5";
+    users.groups.bbb-html5 = {};
+
+    users.users.bbb-soffice.group = "bbb-soffice";
+    users.groups.bbb-soffice = {};
+
+    users.users.bbb-webrtc-sfu.group = "bbb-webrtc-sfu";
+    users.groups.bbb-webrtc-sfu = {};
+
+    users.users.freeswitch.group = "freeswitch";
+    users.groups.freeswitch = {};
+
+    users.users.greenlight.group = "greenlight";
+    users.groups.greenlight = {};
+
+    users.users.kurento.group = "kurento";
+    users.groups.kurento = {};
+
+    users.users.turnserver.group = "turnserver";
+    users.groups.turnserver = {};
 }
